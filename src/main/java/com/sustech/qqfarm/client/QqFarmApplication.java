32b2197be10583c18bf6ea98eb4380986a97b538
fanxychild <100819203+Layheng-Hok@users.noreply.github.com>
2025-11-22 23:45:02 +0800

    Fix UI

--- src/main/java/com/sustech/qqfarm/client/QqFarmApplication.java
+++ src/main/java/com/sustech/qqfarm/client/QqFarmApplication.java
@@ -6,17 +6,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class QqFarmApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(QqFarmApplication.class.getResource("/com/sustech/qqfarm/qq-farm-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 480, 650);
+        scene.getStylesheets().add(Objects.requireNonNull(QqFarmApplication.class.getResource("/com/sustech/qqfarm/styles.css")).toExternalForm());
         stage.setTitle("QQ Farm Game");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
-
         // Close socket on exit
         stage.setOnCloseRequest(e -> System.exit(0));
     }
@@ -24,4 +26,5 @@ public class QqFarmApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
 }
